d5ee522adaeeb719299e5f457304db157c346274
Lightt <manish.eclipse@gmail.com>
2019-04-20 00:14:29 +0545

    Added bootstraps and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the blog app to the settings
+
+# Create a migration
+# migrate
+# add to the admin
